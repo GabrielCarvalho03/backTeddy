bd07124ab0698a90585119d71934def3ec0cc44b
GabrielCarvalho03 <gabrielcarvalho1734@gmail.com>
2024-11-17 12:39:01 -0300

    import controllers

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,8 +1,8 @@
 import { FastifyInstance } from "fastify";
-import { GetUser } from "@controllers/userController/getUser";
-import { CreateUser } from "@controllers/userController/createUser";
-import { UpdateUser } from "@controllers/userController/updateUser";
-import { RemoveUser } from "@controllers/userController/removeUser";
+import { GetUser } from "../controllers/userController/getUser";
+import { CreateUser } from "../controllers/userController/createUser";
+import { UpdateUser } from "../controllers/userController/updateUser";
+import { RemoveUser } from "../controllers/userController/removeUser";
 
 export const UserRoute = async (app: FastifyInstance) => {
   app.get("/users", GetUser);
